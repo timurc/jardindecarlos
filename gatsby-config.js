d8afd50f8d90ca8a22442e500e89f3a686716d8f
Timur Çelikel <me@timurcelikel.com>
2018-10-28 10:18:07 -0500

    remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,19 +45,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link, graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 class BlogIndex extends React.Component {
   render() {
