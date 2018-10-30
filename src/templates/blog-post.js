1eb739a386b89cf3861fc6076889ee9056ca2179
Timur Çelikel <me@timurcelikel.com>
2018-10-29 21:02:57 -0500

    basic layout for blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { Link, graphql } from 'gatsby';
 import get from 'lodash/get';
+import Parser from 'html-react-parser';
+import s from './style.module.less';
+import './wp-content-style.less';
 
 import Layout from '../components/Layout';
 
@@ -16,29 +19,40 @@ class BlogPostTemplate extends React.Component {
       post.featured_media &&
       post.featured_media.localFile.childImageSharp.fluid;
 
+    const blogPostContent = Parser(post.content);
+
     return (
       <Layout location={this.props.location}>
         <Helmet
           meta={[{ name: 'description', content: siteDescription }]}
           title={`${post.title} | ${siteTitle}`}
         />
-        <article>
-          <header>
+        <article className={s.article}>
+          {featuredImage && (
+            <img
+              className={s.featuredImage}
+              src={featuredImage.base64}
+              srcSet={featuredImage.srcSet}
+            />
+          )}
+          <header className={s.header}>
             <time>{post.date}</time>
-            <h1>{post.title}</h1>
+            <h1 className={s.title}>{post.title}</h1>
           </header>
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          {blogPostContent}
+          <div className={s.prevNext}>
+            {previous && (
+              <Link to={previous.slug} rel="prev">
+                ← {previous.title}
+              </Link>
+            )}
+            {next && (
+              <Link to={next.slug} rel="next">
+                {next.title} →
+              </Link>
+            )}
+          </div>
         </article>
-        {previous && (
-          <Link to={previous.slug} rel="prev">
-            ← {previous.title}
-          </Link>
-        )}
-        {next && (
-          <Link to={next.slug} rel="next">
-            {next.title} →
-          </Link>
-        )}
       </Layout>
     );
   }
