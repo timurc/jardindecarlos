53c1caccd10994ad046fe4b18796b633a05a6ebf
Timur Çelikel <me@timurcelikel.com>
2018-10-31 14:27:05 -0500

    enable acf

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
         // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
         // This feature is untested for sites hosted on Wordpress.com.
         // Defaults to true.
-        useACF: false,
+        useACF: true,
         // Include specific ACF Option Pages that have a set post ID
         // Regardless if an ID is set, the default options route will still be retrieved
         // Must be using V3 of ACF to REST to include these routes
